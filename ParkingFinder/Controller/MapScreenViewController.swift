74e5e14bd1d4264aaf5a4f6a2194e37900dc349f
lilylilyngo <13605973@student.uts.edu.au>
2022-05-09 13:37:20 +1000

    add in cark park API

--- ParkingFinder/Controller/MapScreenViewController.swift
+++ ParkingFinder/Controller/MapScreenViewController.swift
@@ -6,16 +6,34 @@
 //
 
 import Foundation
-
-
 import UIKit
+
+let carParkUrl = "https://api.transport.nsw.gov.au/v1/carpark?facility="
+
 
 class MapScreenViewController: UIViewController {
 
+    let url = URL(string: carParkUrl + "3")!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func carParks() {
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.addValue("apikey fyQ2UwCzxYDSwJRbc58uZkwGoioNfMOs6yVj", forHTTPHeaderField: "Authorization")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
 
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard error == nil else { print(error!.localizedDescription); return }
+            guard let data = data else { print("Empty data"); return }
+
+            if let str = String(data: data, encoding: .utf8) {
+                print(str)
+            }
+        }.resume()
+    }
 
 }

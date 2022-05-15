aa52c9e0f210e104dbe54e20630927effdc7c4c2
lilylilyngo <13605973@student.uts.edu.au>
2022-05-16 00:00:12 +1000

    added in map view and constrains

--- ParkingFinder/Controller/ParkingDetailsViewController.swift
+++ ParkingFinder/Controller/ParkingDetailsViewController.swift
@@ -9,9 +9,11 @@ import Foundation
 
 
 import UIKit
+import MapKit
 
 class ParkingDetailsViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var carparkNameLabel: UILabel!
     
     var carparkName = ""
@@ -28,6 +30,19 @@ class ParkingDetailsViewController: UIViewController {
         
         print(carParkAPIModels.carParks(carparkID))
         
+        setMapView()
+    }
+    
+    func setMapView () {
+        let location = CLLocationCoordinate2D(latitude: -33.69163, longitude: 150.906022)
+        let span = MKCoordinateSpan(latitudeDelta: 0.008, longitudeDelta: 0.008)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+                
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = "Tallawong Station Car Park"
+        mapView.addAnnotation(annotation)
     }
 
 
